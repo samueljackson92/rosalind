24195d56f9b3130c3c2cfe21a3626f6c5ba67e89
Samuel Jackson <samueljackson@outlook.com>
2014-04-05 11:06:17 +0100

    Add solution to RNA problem.

---
+++ src/RNA_transcribing_DNA_to_RNA.hs
@@ -0,0 +1,17 @@
+-- ID:      RNA
+-- Name:    Transcribing DNA into RNA
+-- Author:  Samuel Jackson
+-- Email:   samueljackson@outlook.com
+
+--replace any occurance of T with the letter U
+transcribe :: String -> String
+transcribe xs = map replace xs
+  where
+    replace x
+      | x == 'T' = 'U'
+      | otherwise = x
+
+main:: IO ()
+main = do
+    line <- getLine
+    putStrLn $ transcribe line
